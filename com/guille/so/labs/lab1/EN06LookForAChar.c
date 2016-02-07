ef0341c485716fd06f7cbfecf52980de5e302eb3
Guillermo Facundo Colunga <ZenMaster91@users.noreply.github.com>
2016-02-07 19:34:06 +0100

    Create EN06LookForAChar.c

---
+++ com/guille/so/labs/lab1/EN06LookForAChar.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DEFAULT_CHAR 'a'
+#define DEFAULT_STRING "Fernando Alvarez Garcia"
+
+#define STRING_COMMAND_LINE_POSITION 1
+#define CHAR_COMMAND_LINE_POSITION 2
+
+main (int argc, char *argv[]) {
+  
+  if (argc==1)
+    searchAndCount(DEFAULT_CHAR, DEFAULT_STRING);
+  else
+    if (argc==2)
+      searchAndCount(DEFAULT_CHAR, argv[STRING_COMMAND_LINE_POSITION]);
+    else
+      searchAndCount(argv[CHAR_COMMAND_LINE_POSITION][0], argv[STRING_COMMAND_LINE_POSITION]);
+  exit(1);
+}
+
+void searchAndCount(char character, char *string) {
+  
+  int i, sum=0;
+  int length=strlen(string);
+  
+  printf("Searching in string %s\n",string);
+  for (i=0;i<length;i++)
+    if (string[i]==character){
+      printf("Position %i of the string contains character [%c]\n",i,character);
+      sum++;
+    }
+  
+  printf("The string contains %d a's.\n", sum);
+}
+  
+// 1. Compile using "gcc EN06LookForAChar.c"
+// 2. Execute using "./a.out"
+// 3. Modify the searchAndCount function so that it counts and returns the number of matching characters in the string
